f8bf5c0e511bf0bc4e056f475d462c1bb545eced
BugHunter-X <askzenai@gmail.com>
2024-12-23 19:42:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This bug occurs when using the Expo managed workflow with the react-native-maps library.  The map sometimes fails to render, showing only a blank space. This happens inconsistently, making it difficult to debug.
